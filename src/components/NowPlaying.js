a859e03591309b77801283424864cd2362bce876
sanuiitk65 <sknitp65@gmail.com>
2024-02-06 23:53:41 +0530

    ready to submit

--- src/components/NowPlaying.js
+++ src/components/NowPlaying.js
@@ -5,30 +5,7 @@ import {useState, useEffect, useRef } from 'react';
 const NowPlaying = ({ audioFile, audioRef, handleEnded}) => {
 
     const [isPlaying, setIsPlaying] = useState(false);
-    const [playbackPosition, setPlaybackPosition] = useState(0);
-    // useEffect(() => {
-    //     if(audioRef.current){
-    //         if (isPlaying) {
-    //             audioRef.current.currentTime = playbackPosition;
-    //             audioRef.current.play();
-                
-    //           } else {
-    //             audioRef.current.pause();
-    //             //console.log(audioRef.current)
-    //           }
-    //     }
-    //   }, [isPlaying, playbackPosition]);
-
-    //   const handleEnded = () => {
-    //     if (currentTrackIndex < audioFiles.length - 1) {
-    //       setCurrentTrackIndex(currentTrackIndex + 1);
-    //     } else {
-    //       setCurrentTrackIndex(0);
-    //     }
-    //   };
-
-
-
+    
   return (
     <div className='mx-2'>
       <h2>Now Playing</h2>
